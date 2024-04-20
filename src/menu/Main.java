6befb503b9a33325de570d9290294d80f07addaf
DvdPires19 <david.pires1997@gmail.com>
2024-04-20 13:48:17 +0200

    Proyecto creado. Clases Cine, Peliculas, Salas y Main añadidas

---
+++ src/menu/Main.java
@@ -0,0 +1,11 @@
+package menu;
+import clases.Salas;
+import clases.Cine;
+import clases.Peliculas;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    }
+
