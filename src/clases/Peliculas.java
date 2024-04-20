f873d787462bbf5f0d466d3160efc8faa0bae10b
DvdPires19 <david.pires1997@gmail.com>
2024-04-20 13:49:14 +0200

    Clase Peliculas modificada. Añadidos atributos, constructor, getters y setters

--- src/clases/Peliculas.java
+++ src/clases/Peliculas.java
@@ -1,1 +1,16 @@
+package clases;
+public class Peliculas {
+    private String titulo;
+    private int duracion;
+    public Peliculas(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
 
+    public int getDuracion() {
+        return duracion;
+    }
+}
